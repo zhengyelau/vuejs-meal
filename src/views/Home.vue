f5a82294a07670b395ed99381f397743c23f9e5e
Zheng Ye <zhengyelau@hotmail.com>
2024-08-18 22:21:03 +0800

    Add meal list search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,22 @@
 import { computed } from 'vue';
 import store from "../store"
 const meal = computed(() => store.state.meals)
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 </script>
 
 <template>
-    Home
-    <pre>{{ meal }}</pre>
+    <div class="flex flex-col p-8">
+
+        <div>
+            <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals" />
+        </div>
+
+        <div class="flex gap-2 mt-2 justify-center">
+            <router-link :to="{name: 'byLetter', params: {letter: letter}}" v-for="letter in letters.split('')" :key="letter">
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
 </template>
---
+++ src/views/MealList.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        Meal list
+    </div>
+</template>
